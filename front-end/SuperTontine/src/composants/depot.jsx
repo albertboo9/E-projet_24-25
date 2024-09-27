008f1d2fefc30e5d993378dd4d4473587c7aa4ac
albertboo9 <booalbert60@gmail.com>
2024-09-28 00:11:51 +0100

    suppression d'un dossier inutile

--- front-end/SuperTontine/src/composants/depot.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Depot() {
-  return (
-    <div>
-      <h1>Les dépôts seront effectué via ce composant</h1>
-    </div>
-  )
-}
--- front-end/SuperTontine/src/composants/retrait.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Retrait() {
-  return (
-    <div>
-      <h1>Les retraits seront effectués via ce composant</h1>
-    </div>
-  )
-}
